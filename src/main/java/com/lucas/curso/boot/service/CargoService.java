0652e641f46326d789674352fd97918fb771b44e
ldunhao <ldunhao@hotmail.com>
2021-06-25 15:16:05 -0300

    Add CargoService

---
+++ src/main/java/com/lucas/curso/boot/service/CargoService.java
@@ -0,0 +1,18 @@
+package com.lucas.curso.boot.service;
+
+import com.lucas.curso.boot.domain.Cargo;
+
+import java.util.List;
+
+public interface CargoService {
+
+    void salver(Cargo cargo);
+
+    void editar(Cargo cargo);
+
+    void excluir(Long id);
+
+    Cargo buscarPorId(Long id);
+
+    List<Cargo> buscarTodos();
+}
---
+++ src/main/java/com/lucas/curso/boot/service/CargoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.lucas.curso.boot.service;
+
+import com.lucas.curso.boot.dao.CargoDao;
+import com.lucas.curso.boot.domain.Cargo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = false)
+public class CargoServiceImpl implements CargoService{
+
+    @Autowired
+    private CargoDao dao;
+
+    @Override
+    public void salver(Cargo cargo) {
+        dao.save(cargo);
+    }
+
+    @Override
+    public void editar(Cargo cargo) {
+        dao.update(cargo);
+    }
+
+    @Override
+    public void excluir(Long id) {
+        dao.delete(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cargo buscarPorId(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cargo> buscarTodos() {
+        return dao.findAll();
+    }
+}
